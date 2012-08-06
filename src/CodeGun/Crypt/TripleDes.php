0af16e03f57b50b2bfb44846c28de3a9ff1146ff
hfcorriez <hfcorriez@gmail.com>
2012-08-06 14:41:57 +0800

    Use MCRYPT_MODE_ECB instance MCRYPT_MODE_CBC

--- src/CodeGun/Crypt/TripleDes.php
+++ src/CodeGun/Crypt/TripleDes.php
@@ -13,14 +13,14 @@ class TripleDes
         $block_size = mcrypt_get_block_size('tripledes', 'ecb');
         $padding_char = $block_size - (strlen($input) % $block_size);
         $src_data .= str_repeat(chr($padding_char), $padding_char);
-        return mcrypt_encrypt(MCRYPT_3DES, $key, $src_data, MCRYPT_MODE_CBC, $iv);
+        return mcrypt_encrypt(MCRYPT_3DES, $key, $src_data, MCRYPT_MODE_ECB, $iv);
     }
 
     public static function decrypt($input, $key, $iv)
     {
         $key = pack('H48', $key);
         $iv = pack('H16', $iv);
-        $result = mcrypt_decrypt(MCRYPT_3DES, $key, $input, MCRYPT_MODE_CBC, $iv);
+        $result = mcrypt_decrypt(MCRYPT_3DES, $key, $input, MCRYPT_MODE_ECB, $iv);
         $end = ord(substr($result, -1));
         $out = substr($result, 0, -$end);
         return $out;
